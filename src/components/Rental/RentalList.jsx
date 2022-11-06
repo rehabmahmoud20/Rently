18a55215c373e6bd4da26275c58b8ed891c01ad7
hesham <heshamali1995@hotmail.com>
2022-11-06 17:53:54 +0200

    Global Styles Added

--- src/components/Rental/RentalList.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const RentalList = () => {
-  return (
-    <div>RentalList</div>
-  )
-}
-
-export default RentalList
---
+++ src/components/Rental/RentalList/RentalList.jsx
@@ -0,0 +1,11 @@
+import "./rentalList.css";
+
+const RentalList = () => {
+  return (
+    <div>
+      Rental List
+    </div>
+  )
+}
+
+export default RentalList
