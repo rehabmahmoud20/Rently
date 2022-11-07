159fbd3fcaf9f4c44fc34a8a4ad2143e1b183f18
ruaa <ruaa.reda99@gmail.com>
2022-11-07 20:37:42 +0200

    add rentaldetails

--- src/App.js
+++ src/App.js
@@ -1,56 +1,40 @@
 // global styles
-import './Sass/Style.css';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import "./Sass/Style.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
-import Signup from './components/Auth/Signup';
-import Signin from './components/Auth/Signin';
-import ForgetPassword from './components/Auth/ForgetPassword';
-import Profile from './components/Profile/Profile';
-import Favourits from './components/Favourits/Favourits';
-import Home from './components/Home/Home';
-import RentalDetails from './components/Rental/RentalDetails';
-import RentalList from './components/Rental/RentalList/RentalList';
-import FAQ from './components/FAQ/FAQ';
-import About from './components/About/About';
-import AddRental from './components/AddRental/AddRental';
-import NavbarComponent from './components/Shared/NavbarComponent';
-import Footer from './components/Shared/Footer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./components/Auth/Signup";
+import Signin from "./components/Auth/Signin";
+import ForgetPassword from "./components/Auth/ForgetPassword";
+import Profile from "./components/Profile/Profile";
+import Favourits from "./components/Favourits/Favourits";
+import Home from "./components/Home/Home";
+import RentalDetails from "./components/Rental/RentalDetails/RentalDetails";
+import RentalList from "./components/Rental/RentalList/RentalList";
+import FAQ from "./components/FAQ/FAQ";
+import About from "./components/About/About";
+import AddRental from "./components/AddRental/AddRental";
+import NavbarComponent from "./components/Shared/NavbarComponent";
+import Footer from "./components/Shared/Footer";
 function App() {
   return (
     <BrowserRouter>
       <NavbarComponent />
       <main>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='signin' element={<Signin />} />
-          <Route path='signup' element={<Signup />} />
-          <Route
-            path='forget-password'
-            element={<ForgetPassword />}
-          />
-          <Route path='favourits' element={<Favourits />} />
-          <Route path='profile/*' element={<Profile />} />
-          <Route
-            path='rental-details'
-            element={<RentalDetails />}
-          />
-          <Route
-            path='rental-list'
-            element={<RentalList />}
-          />
-          <Route
-            path='add-rental'
-            element={<AddRental />}
-          />
+          <Route path="/" element={<Home />} />
+          <Route path="signin" element={<Signin />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="forget-password" element={<ForgetPassword />} />
+          <Route path="favourits" element={<Favourits />} />
+          <Route path="profile/*" element={<Profile />} />
+          <Route path="rental-details" element={<RentalDetails />} />
+          <Route path="rental-list" element={<RentalList />} />
+          <Route path="add-rental" element={<AddRental />} />
 
-          <Route path='about' element={<About />} />
-          <Route path='FAQ' element={<FAQ />} />
+          <Route path="about" element={<About />} />
+          <Route path="FAQ" element={<FAQ />} />
         </Routes>
         {/* toast container "alert from react toastify" don't add it again ❗*/}
         <ToastContainer autoClose={5000} />
--- src/components/Rental/RentalDetails.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const RentalDetails = () => {
-  return (
-    <div>RentalDetails</div>
-  )
-}
-
-export default RentalDetails;
