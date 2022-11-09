bef5eb3a9c130333f44e11ff4ea86ad7495535bf
rehabmahmoud20 <rehabgendy16@gmail.com>
2022-11-09 22:22:42 +0200

    edit navbar

--- src/components/Shared/NavbarComponent.jsx
+++ src/components/Shared/NavbarComponent.jsx
@@ -1,7 +1,11 @@
+import userPic from "../Profile/images/user-avatar.png";
+
 import { Navbar } from "flowbite-react";
 import { UseAuthStatus } from "../Hooks/useAuthStatus";
 import rently from "../../assets/images/rently.png";
 import { useSelector } from "react-redux";
+
+
 
 // icons
 import { FaUserCircle } from "react-icons/fa";
@@ -9,26 +13,27 @@ import { Link } from "react-router-dom";
 // Link
 
 const NavbarComponent = () => {
+  const userData = { ...useSelector((state) => state.user.userData) };
   const { isLoggedIn } = UseAuthStatus();
   const globalAuthState = useSelector(
     (state) => state.authentication.isLoggedin
   );
   return (
-    <div>
       <Navbar
         fluid={true}
         rounded={true}
-        className="md:border-b-2  rounded-none md-divide-gray-200 container mx-auto"
+        className="md:border-b-2  rounded-none md-divide-gray-200 items-center  mx-auto"
       >
         <Link to="/">
           <img src={rently} className="mr-3  w-15 h-10" alt="Flowbite Logo" />
         </Link>
 
         <Navbar.Toggle />
-        <Navbar.Collapse>
+        <Navbar.Collapse className="items-center">
+
           <Link
             to="/rental-list"
-            className="  font-bold py-3 border-b-2 divide-gray-200 md:border-none md:py-0 dark:text-white"
+            className="h-fit text-lg  font-bold py-3 border-b-2 divide-gray-200 md:border-none  dark:text-white"
           >
             Rent
           </Link>
@@ -37,43 +42,50 @@ const NavbarComponent = () => {
             <>
               <Link
                 to="/add-rental"
-                className=" font-bold py-3 border-b-2 divide-gray-200 md:border-none md:py-0 dark:text-white"
+                className="h-fit text-lg font-bold py-3 border-b-2 divide-gray-200 md:border-none  dark:text-white"
               >
                 Manage rentals
               </Link>
               <Link
                 to="/favourits"
-                className=" font-bold py-3 border-b-2 divide-gray-200 md:border-none md:py-0 dark:text-white"
+                className="h-fit text-lg font-bold py-3 border-b-2 divide-gray-200 md:border-none dark:text-white"
               >
                 Favourites
               </Link>
               <Link
                 to="/profile"
-                className="font-bold py-3 border-b-2 divide-gray-200 md:border-none md:py-0 dark:text-white"
+                className="h-fit font-bold py-3  divide-gray-200  md:py-0  hight-fit dark:text-white"
               >
-                <FaUserCircle className="text-gray-600 text-2xl " />
+                  <img
+          className="w-10 h-10 object-cover rounded-full transition-all duration-500"
+          src={userData.avatar ? userData.avatar : userPic}
+          alt="profile"
+        />
               </Link>
             </>
           ) : (
             <>
               <Link
                 to="/signin"
-                className=" font-bold py-3 border-b-2 divide-gray-200 md:border-none md:py-0"
+                className="h-fit  text-lg font-bold py-3 border-b-2 divide-gray-200 md:border-none "
               >
                 Sign in
               </Link>
 
               <Link
                 to="/signup"
-                className=" font-bold py-3 border-b-2 divide-gray-200 md:border-none md:py-0 dark:text-white"
+                className="h-fit  text-lg font-bold py-3  divide-gray-200   dark:text-white"
               >
                 Sign up
               </Link>
             </>
           )}
         </Navbar.Collapse>
+
+         
+        {/* </Navbar.Collapse> */}
       </Navbar>
-    </div>
+    // </div>
   );
 };
 
